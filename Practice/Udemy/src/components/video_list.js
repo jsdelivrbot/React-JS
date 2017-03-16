e305f4a5e310ddfc0238a1dff189500e7097bda9
meghna-saxena <meghna.s1906@gmail.com>
2017-03-16 10:24:18 +0530

    introduced props

--- Practice/Udemy/src/components/video_list.js
+++ Practice/Udemy/src/components/video_list.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 
 //define function
-const VideoList = () => {
+const VideoList = (props) => {
+	//similar to const videos = props.videos; (this is an array of videos)
 	return (
-	<ul className = "col-md-4 list-group"> 
-	</ul>
-	);
-};
+		<ul className = "col-md-4 list-group"> 
+		{props.videos.length}
+		</ul>
+		);
+	};
 
 
-export default VideoList;
+	export default VideoList;
--- Practice/Udemy/src/index.js
+++ Practice/Udemy/src/index.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyCGMyIVw9YmF43dLDd0RzFDGylEcjW07WU'
 
@@ -44,6 +45,12 @@ class App extends Component {
 // });
 // }
 
+
+//=========================================================================================
+
+
+
+
 //or drop function(data) and use arrow syntax
 
 YTSearch ({key: API_KEY, term:'surfboards'}, (videos) => {
@@ -60,15 +67,14 @@ render() {
 	return (
 		<div>
 		<SearchBar />
+		<VideoList videos = {this.state.videos} /> 
 		</div>
 		);
 	}
 }
 
-
-
-
-
+//In <VideoList videos = {this.state.videos} /> 
+//we are passing props from parent component to child.
 
 //Take this component's generated HTML, and put it on the page (in the DOM)
 
