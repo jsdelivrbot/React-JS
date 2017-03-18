1e45a9240de4ba0cbf79b99be00a71e182c1b8e3
meghna-saxena <meghna.s1906@gmail.com>
2017-03-18 16:59:12 +0530

    BookDetail container

--- Practice/redux/src/reducers/reducer_active_book.js
+++ Practice/redux/src/reducers/reducer_active_book.js
@@ -3,9 +3,10 @@
 
 
 //state = null  means if state is undefined, set it to 'null'
+
 export default function (state = null, action) {
 	switch (action.type) {
-		case: 'BOOK_SELECTED':
+		case 'BOOK_SELECTED':
 		return action.payload;
 	}
 
