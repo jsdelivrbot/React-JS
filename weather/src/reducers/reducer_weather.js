f0dacd879044c622556a204be064316b9d60d5c6
meghna-saxena <meghna.s1906@gmail.com>
2017-03-21 12:25:07 +0530

    avoiding state mutation in reducers

--- weather/src/reducers/reducer_weather.js
+++ weather/src/reducers/reducer_weather.js
@@ -1,6 +1,24 @@
-// reducers are functtion which returns state
+import { FETCH_WEATHER } from '../actions/index';
 
-export default function(state=null, action) {
+
+// reducers are function which returns state
+
+//set the initial state to an array instead of null
+export default function(state=[], action) { 
 	// console.log('Action received', action);
+	switch(action.type) {
+		case FETCH_WEATHER:
+
+		// return state.push(action.payload.data) // wrong approach, it manipulates existing array
+
+		// return state.concat([action.payload.data]); OR we can also write like -
+		return [action.payload.data, ...state]; //[city,city,city] NOT [city, [city,city]]
+	}
 	return state;
 }
+
+/*Avoiding State mutation in reducers-
+
+Concat doesn't change the old array
+it makes a new array with old and new stuff added
+we arenot mutating state, we are creating new version of state /*
