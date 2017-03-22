323373ab503935c0ff9cccf1faeaf6f3ef372de7
meghna-saxena <meghna.s1906@gmail.com>
2017-03-22 16:28:27 +0530

    using react router and redux forms

---
+++ Blog-App/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Router, browserHistory } from 'react-router';
+import reducers from './reducers';
+import routes from './routes';
+// import App from './components/app'; we delete this line 
+//since we want router to render rather than app
+
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <Router history = { browserHistory } routes={routes} />
+  </Provider>
+  , document.querySelector('.container'));
