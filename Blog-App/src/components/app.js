f3f8c6fd216d9be8fb8029fd427f3504fcce9cfd
meghna-saxena <meghna.s1906@gmail.com>
2017-03-22 18:20:49 +0530

    nesting of routes

--- Blog-App/src/components/app.js
+++ Blog-App/src/components/app.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
+//child component of app is Greeting
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+      	{this.props.children} 
+      </div>
     );
   }
 }
+
--- Blog-App/src/routes.js
+++ Blog-App/src/routes.js
@@ -3,8 +3,25 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
 
+const Greeting = () => {
+	return <div>Hey there!</div>;
+};
+
 export default (
-	<Route path="/" component={App} />
+	<Route path="/" component={App}>
+		<Route path="greet" component={Greeting} />
+		<Route path="greet2" component={Greeting} />
+		<Route path="greet3" component={Greeting} />
+	</Route>
 );
 
 //google.com/ => renders App
+
+//=======================================Nesting of Routes=======================================
+//valid nest (stacks) of routes -
+//mapping b/w routes and components -
+
+// /			App
+// /greet		App, Greeting
+// /greet2		App, Greeting	
+// /greet3		App, Greeting
