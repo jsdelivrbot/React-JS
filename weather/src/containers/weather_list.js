ecf85962374eebd3a33bb0ff50f45574f347432d
meghna-saxena <meghna.s1906@gmail.com>
2017-03-22 11:51:04 +0530

    labelling of units of chart

--- weather/src/containers/weather_list.js
+++ weather/src/containers/weather_list.js
@@ -9,13 +9,18 @@ class WeatherList extends Component {
 		const name = cityData.city.name;
 		const temps = cityData.list.map(weather=>weather.main.temp);
 		// console.log(temps); //confirms an array of number(temp)
+	//================================================================================
+		//for conversion of unit of temp -
+		// const temps = _.map(cityData.list.map(weather=>weather.main.temp), (temp) => temp-273);
+		const pressures = cityData.list.map(weather=>weather.main.pressure);
+		const humidities = cityData.list.map(weather=>weather.main.humidity);
 
 		return (
 			<tr key={name}>
 				<td>{name}</td>
-				<td>
-				<Chart data={temps} color="orange" />
-				</td>
+				<td><Chart data={temps} color="orange" units="K" /></td>
+				<td><Chart data={pressures} color="green" units="hPa" /></td>
+				<td><Chart data={humidities} color="black" units="%" /></td>
 			</tr>
 		);
 	}
@@ -27,9 +32,9 @@ class WeatherList extends Component {
 				<thead>
 					<tr>
 						<th>City</th>
-						<th>Temperature</th>
-						<th>Pressure</th>
-						<th>Humidity</th>
+						<th>Temperature (K)</th>
+						<th>Pressure (hPa)</th>
+						<th>Humidity (%)</th>
 					</tr>
 				</thead>
 
