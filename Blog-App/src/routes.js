eafbc6ecaef5047ac3346efcc0cb34b1d953c705
meghna-saxena <meghna.s1906@gmail.com>
2017-03-23 11:37:09 +0530

    managing indexroute with react-router

--- Blog-App/src/routes.js
+++ Blog-App/src/routes.js
@@ -2,26 +2,24 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
+import PostsIndex from './components/posts_index';
 
-const Greeting = () => {
-	return <div>Hey there!</div>;
-};
+// const Greeting = () => {
+// 	return <div>Hey there!</div>;
+// };
+
+// export default (
+// 	<Route path="/" component={App}>
+// 	<IndexRoute component={PostsIndex} />
+// 		<Route path="greet" component={Greeting} />
+// 		<Route path="greet2" component={Greeting} />
+// 		<Route path="greet3" component={Greeting} />
+// 	</Route>
+// );
+
 
 export default (
 	<Route path="/" component={App}>
-		<Route path="greet" component={Greeting} />
-		<Route path="greet2" component={Greeting} />
-		<Route path="greet3" component={Greeting} />
+	<IndexRoute component={PostsIndex} />
 	</Route>
 );
-
-//google.com/ => renders App
-
-//=======================================Nesting of Routes=======================================
-//valid nest (stacks) of routes -
-//mapping b/w routes and components -
-
-// /			App
-// /greet		App, Greeting
-// /greet2		App, Greeting	
-// /greet3		App, Greeting
