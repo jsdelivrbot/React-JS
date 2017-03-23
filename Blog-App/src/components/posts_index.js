55c9bbdfa74e8813c4bfa3bb3a9725f76a57187c
meghna-saxena <meghna.s1906@gmail.com>
2017-03-23 14:15:34 +0530

    made PostsNew component and routed its pathway to posts/new

--- Blog-App/src/components/posts_index.js
+++ Blog-App/src/components/posts_index.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// import { bindActionCreators } from 'redux'; //we dont need this line anymore
+import { fetchPosts } from '../actions/index';
 
 // export default () => {
 // 	return <div>List of blog posts.</div>;
 // };
 
-//Refactoring a function component to a class component-
+//Refactoring a function component to a class component- === 
+// ======which ultimately is changed into a container
 
 class PostsIndex extends Component {
 
 	componentWillMount() {
-		console.log('this would be a good time to call action creator to fetch posts');
+		this.props.fetchPosts();
 	}
 
 	render() {
@@ -19,4 +23,23 @@ class PostsIndex extends Component {
 	}
 }
 
-export default PostsIndex;
+// function mapDispatchToProps(dispatch) {
+// 	return bindActionCreators ({ fetchPosts }, dispatch);
+// }
+
+// //since first argument should be state in connect, and wwe don't have it yet,
+// // we will assign it 'null'
+
+// export default connect (null, mapDispatchToProps) (PostsIndex);
+
+//========================================================
+//============================================================
+
+//we can avoid writing boilerplate mapDispatchToProps in following manner -
+//just write an export line as follows-
+
+// export default connect (null, { fetchPosts: fetchPosts }) (PostsIndex);
+
+//OR
+
+export default connect (null, { fetchPosts }) (PostsIndex);
--- Blog-App/src/routes.js
+++ Blog-App/src/routes.js
@@ -3,6 +3,8 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
+
 
 // const Greeting = () => {
 // 	return <div>Hey there!</div>;
@@ -21,5 +23,6 @@ import PostsIndex from './components/posts_index';
 export default (
 	<Route path="/" component={App}>
 	<IndexRoute component={PostsIndex} />
+		<Route path="posts/new" component={PostsNew} />
 	</Route>
 );
