d92437bcb8af60237731a852cb3ea4f09f2e5cb6
meghna-saxena <meghna.s1906@gmail.com>
2017-03-23 20:25:05 +0530

    Posts index

--- Blog-App/src/components/posts_index.js
+++ Blog-App/src/components/posts_index.js
@@ -17,6 +17,18 @@ class PostsIndex extends Component {
 		this.props.fetchPosts();
 	}
 
+	renderPosts() {
+		return this.props.posts.map((post) => {
+			return (
+				<li className="list-group-item" key={post.id}>
+				<span className="pull-xs-right">{post.categories}</span>
+				<strong>{post.title}</strong>
+				</li>
+			);
+		});
+	}
+
+
 	render() {
 		return (
 			<div>
@@ -25,7 +37,11 @@ class PostsIndex extends Component {
 					Add a post
 				</Link>
 				</div>
-				List of blog posts.
+				<h3>Posts</h3>
+				<ul className="list-group">
+				{this.renderPosts()}
+				</ul>
+				
 			</div>
 		);
 	}
@@ -43,11 +59,21 @@ class PostsIndex extends Component {
 //========================================================
 //============================================================
 
+
+
 //we can avoid writing boilerplate mapDispatchToProps in following manner -
 //just write an export line as follows-
 
 // export default connect (null, { fetchPosts: fetchPosts }) (PostsIndex);
 
 //OR
+//======================================================================
 
-export default connect (null, { fetchPosts }) (PostsIndex);
+// export default connect (null, { fetchPosts }) (PostsIndex);
+
+
+function mapStateToProps(state) {
+	return {posts : state.posts.all};
+}
+
+export default connect (mapStateToProps, { fetchPosts }) (PostsIndex);
