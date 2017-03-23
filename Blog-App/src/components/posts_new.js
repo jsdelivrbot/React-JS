e64889d642e757a0bc6dce4b665bec062cd4cf12
meghna-saxena <meghna.s1906@gmail.com>
2017-03-23 15:31:08 +0530

    made a form element with all the input fields

--- Blog-App/src/components/posts_new.js
+++ Blog-App/src/components/posts_new.js
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
+import { reduxForm }  from 'redux-form';
 
 class PostsNew extends Component {
 	render() {
 		return (
-			<div>Create form</div>
+			<form>
+				<h3>Create A New Post</h3>
+				<div className="form-group">
+					<label>Title</label>
+					<input type="text" className="form-control" />
+				</div>
+
+				<div className="form-group">
+					<label>Categories</label>
+					<input type="text" className="form-control" />
+				</div>
+
+				<div className="form-group">
+					<label>Content</label>
+					<textarea className="form-control" />
+				</div>
+
+				<button type="submit" className="btnn btn-primary">Submit</button>
+			</form>
 		); 
 	}
 }
 
-export default PostsNew;
+export default reduxForm ({
+	form: 'PostsNewForm',
+	fields: ['title','categories','content']
+
+}) (PostsNew);
+
+
+
+
+
+
+//===============================================================================
+//===============================================================================
+//user types something, which gets recorded on state
+// state === {
+// 	form: {
+// 		PostsNewForm: {
+// 			title: '.....'
+// 			categories: '....'
+// 			content: '......'
+// 		}
+// 	}
+// }
