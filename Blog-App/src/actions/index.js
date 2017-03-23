6676bf12bf0bc78ea7c688d97425283c1f8526a2
meghna-saxena <meghna.s1906@gmail.com>
2017-03-23 13:08:41 +0530

    Imported axios and redux-promise. Also, made an action creator fetchPosts to create an action

---
+++ Blog-App/src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+//define action.type
+
+export const FETCH_POSTS = 'FETCH_POSTS';
+
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=gheukcherwatds'
+
+export function fetchPosts() {
+	const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+	return {
+		type: FETCH_POSTS,
+		payload: request
+	};
+}
--- Blog-App/src/index.js
+++ Blog-App/src/index.js
@@ -5,11 +5,12 @@ import { createStore, applyMiddleware } from 'redux';
 import { Router, browserHistory } from 'react-router';
 import reducers from './reducers';
 import routes from './routes';
+import promise from 'redux-promise';
 // import App from './components/app'; we delete this line 
 //since we want router to render rather than app
 
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
