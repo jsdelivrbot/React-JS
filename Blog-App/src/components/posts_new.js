19568ed2b2f026a9ffeef47e609f899969d2408c
meghna-saxena <meghna.s1906@gmail.com>
2017-03-23 17:46:42 +0530

    passing control to redux form

--- Blog-App/src/components/posts_new.js
+++ Blog-App/src/components/posts_new.js
@@ -3,39 +3,41 @@ import { reduxForm }  from 'redux-form';
 
 class PostsNew extends Component {
 	render() {
+		const {fields:{title,categories,content}, handleSubmit} = this.props;
+		// similar to - 
+		// const handleSublit = this.props.handleSubmit;
+		//const title = this.props.fields.title;
+
+
 		return (
-			<form>
+			<form onSubmit = {handleSubmit}>
 				<h3>Create A New Post</h3>
 				<div className="form-group">
 					<label>Title</label>
-					<input type="text" className="form-control" />
+					<input type="text" className="form-control" {...title} /> //Destructuring of object
+					//We are destructuring the object, so Redux form can entirely control input tag 
 				</div>
 
 				<div className="form-group">
 					<label>Categories</label>
-					<input type="text" className="form-control" />
+					<input type="text" className="form-control" {...categories} />
 				</div>
 
 				<div className="form-group">
 					<label>Content</label>
-					<textarea className="form-control" />
+					<textarea className="form-control" {...content} />
 				</div>
 
-				<button type="submit" className="btnn btn-primary">Submit</button>
+				<button type="submit" className="btn btn-primary">Submit</button>
 			</form>
 		); 
 	}
 }
-
+	
 export default reduxForm ({
 	form: 'PostsNewForm',
 	fields: ['title','categories','content']
-
 }) (PostsNew);
-
-
-
-
 
 
 //===============================================================================
@@ -47,6 +49,6 @@ export default reduxForm ({
 // 			title: '.....'
 // 			categories: '....'
 // 			content: '......'
+// 			}
 // 		}
-// 	}
 // }
