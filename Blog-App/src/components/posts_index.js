d419fb4205414ff109074e59d90c126c8c3a9db6
meghna-saxena <meghna.s1906@gmail.com>
2017-03-23 13:41:44 +0530

    fetching data with lifecycle methods

--- Blog-App/src/components/posts_index.js
+++ Blog-App/src/components/posts_index.js
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-	return <div>List of blog posts.</div>;
-};
+// export default () => {
+// 	return <div>List of blog posts.</div>;
+// };
+
+//Refactoring a function component to a class component-
+
+class PostsIndex extends Component {
+
+	componentWillMount() {
+		console.log('this would be a good time to call action creator to fetch posts');
+	}
+
+	render() {
+		return (
+			<div>List of blog posts.</div>
+		);
+	}
+}
+
+export default PostsIndex;
