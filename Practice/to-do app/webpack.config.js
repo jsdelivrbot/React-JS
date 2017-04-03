ce51c0246435a3f85adafdc33b8b93f827eb3fa1
meghna-saxena <meghna.s1906@gmail.com>
2017-04-03 17:28:06 +0530

    added todolist component

--- Practice/to-do app/webpack.config.js
+++ Practice/to-do app/webpack.config.js
@@ -2,13 +2,13 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './src/index.html',
+  template: './index.html',
   filename: 'index.html',
   inject: 'body'
 })
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './index.js',
   output: {
     path: path.resolve('dist'),
     filename: 'index_bundle.js'
