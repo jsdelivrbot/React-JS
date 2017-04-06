a3c7da4ddf66ec080435d67a5ef506702d5b9a00
meghna-saxena <meghna.s1906@gmail.com>
2017-04-06 17:12:04 +0530

    fixed the webpack-dev-server file

--- ReactProjects/reduxTodos/server.js
+++ ReactProjects/reduxTodos/server.js
@@ -1,39 +1,25 @@
-// var webpack = require('webpack')
-// var webpackDevMiddleware = require('webpack-dev-middleware')
-// var webpackHotMiddleware = require('webpack-hot-middleware')
-// var config = require('./webpack.config')
+var webpack = require('webpack')
+var webpackDevMiddleware = require('webpack-dev-middleware')
+var webpackHotMiddleware = require('webpack-hot-middleware')
+var config = require('./webpack.config')
 
-// var app = new (require('express'))()
-// var port = 3000
+var app = new (require('express'))()
+var port = 3000
 
-// var compiler = webpack(config)
-// app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
-// app.use(webpackHotMiddleware(compiler))
+var compiler = webpack(config)
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
+app.use(webpackHotMiddleware(compiler))
 
-// app.get("/", function(req, res) {
-//   res.sendFile(__dirname + '/index.html')
-// })
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
 
-// app.listen(port, function(error) {
-//   if (error) {
-//     console.error(error)
-//   } else {
-//     console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
-//   }
-// })
+app.listen(port, function(error) {
+  if (error) {
+    console.error(error)
+  } else {
+    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
+  }
+})
 
 
-const express = require ('express');
-const path = require ('path');
-const port = process.env.PORT || 3000;
-const app = express();
-
-app.use(express.static(__dirname));
-
-app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'index.html'));
-});
-
-app.listen(port);
-
-console.log('Server started');
