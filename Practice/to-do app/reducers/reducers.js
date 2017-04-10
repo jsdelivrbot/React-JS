06e7cce84c974090882a9613f71b4bdec9c53bd2
meghna-saxena <meghna.s1906@gmail.com>
2017-04-10 20:44:27 +0530

    made changes

--- Practice/to-do app/reducers/reducers.js
+++ Practice/to-do app/reducers/reducers.js
@@ -3,13 +3,13 @@ import { ADD_TODO } from '../actions/actions'
 
 function todo(state, action) {
    switch (action.type) {
-	
+     
       case ADD_TODO:
-         return {
-            id: action.id,
-            text: action.text,
-         }
-			
+      return {
+         id: action.id,
+         text: action.text,
+      }
+      
       default:
       return state
    }
@@ -17,20 +17,20 @@ function todo(state, action) {
 
 function todos(state = [], action) {
    switch (action.type) {
-	
+     
       case ADD_TODO:
-         return [
-            ...state,
-            todo(undefined, action)
-         ]
-			
+      return [
+      ...state,
+      todo(undefined, action)
+      ]
+      
       default:
       return state
    }
 }
-
 const todoApp = combineReducers({
    todos
 })
+
 
 export default todoApp
