ef47b8a269b06385953c481ff606d15db943efca
meghna-saxena <meghna.s1906@gmail.com>
2017-09-14 16:25:30 +0530

    wired up CommentBox to saveComment action creator

--- ReactTesting/test/actions/index_test.js
+++ ReactTesting/test/actions/index_test.js
@@ -1,6 +1,6 @@
 import { expect } from '../test_helper';
 import { SAVE_COMMENT } from '../../src/actions/types';
-import { save_comment } from '../../src/actions';
+import { saveComment } from '../../src/actions';
 
 describe ('actions', () => {
 	describe ('saveComment', () => {
