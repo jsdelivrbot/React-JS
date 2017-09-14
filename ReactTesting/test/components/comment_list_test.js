01a800856d3a62063b105a125abc9e00ba05f48b
meghna-saxena <meghna.s1906@gmail.com>
2017-09-14 13:38:26 +0530

    expectations on comment list

--- ReactTesting/test/components/comment_list_test.js
+++ ReactTesting/test/components/comment_list_test.js
@@ -2,5 +2,19 @@ import { renderComponent, expect } from '../test_helper';
 import CommentList from '../../src/components/comment_list';
 
 describe ('CommentList', () => {
-	
-})
+	let component;
+
+	beforeEach (() => {
+		const props = { comment:['New comment', 'Other new comment'] };
+		component = renderComponent(CommentList, null, props);
+	});
+
+	it ('shows an LI for each comment', () => {
+		expect (component.find('li').length).to.equal(2);
+	});
+
+	it ('shows each comment that is provided', () => {
+		expect (commponent).to.contain('New comment');
+		expect (commponent).to.contain('Other new comment');
+	});
+});
