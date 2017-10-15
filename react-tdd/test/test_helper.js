2a5db8ada035c881c443846241646163a9b1a26f
meghna-saxena <meghna.s1906@gmail.com>
2017-10-15 11:57:12 +0530

    renamed folder

--- react-tdd/test/test_helper.js
+++ react-tdd/test/test_helper.js
@@ -1,24 +1,22 @@
-import _$ from 'jquery';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
 import jsdom from 'jsdom';
+import _$ from 'jquery';
+import TestUtils from 'react-addons-test-utils';
+import ReactDOM from 'react-dom';
 import chai, { expect } from 'chai';
 import chaiJquery from 'chai-jquery';
+import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../src/reducers';
 
+
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
-global.navigator = global.window.navigator;
-const $ = _$(window);
+const $ = _$(global.window);
 
-chaiJquery(chai, chai.util, $);
-
-function renderComponent(ComponentClass, props = {}, state = {}) {
+function renderComponent(ComponentClass, props, state) {
   const componentInstance =  TestUtils.renderIntoDocument(
-    <Provider store={createStore(reducers, state)}>
+  <Provider store={createStore(reducers, state)}>
       <ComponentClass {...props} />
     </Provider>
   );
@@ -27,10 +25,12 @@ function renderComponent(ComponentClass, props = {}, state = {}) {
 }
 
 $.fn.simulate = function(eventName, value) {
-  if (value) {
+   if (value) {
     this.val(value);
   }
   TestUtils.Simulate[eventName](this[0]);
 };
 
-export {renderComponent, expect};
+chaiJquery(chai, chai.util, $);
+
+export { renderComponent, expect };
